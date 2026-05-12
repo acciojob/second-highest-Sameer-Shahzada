14e2882f01ec97fd02537783f184cec56a7e4ec7
Mohd. Sameer <66036809+Sameer-Shahzada@users.noreply.github.com>
2026-05-12 22:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  // Edge case
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+
+    // Update highest and second highest
+    if (num > first) {
+      second = first;
+      first = num;
+    }
+    // Update only second highest
+    else if (num > second && num !== first) {
+      second = num;
+    }
+  }
+
+  return second;
+}
